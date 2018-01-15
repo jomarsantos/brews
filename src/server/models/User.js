51699d69d56afcf1cea5378f6609a8d173f63738
Jomar Santos <jomaroliversantos@gmail.com>
2018-01-14 23:10:07 -0800

    facebook login logic backend

---
+++ src/server/models/User.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+  name: {type: String, required: true},
+	fbUserId: {type: String, required: true},
+	favorites: [{type: Schema.ObjectId, ref: 'Brew', required: true}]
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- src/server/routes/facebookLogin.js
+++ src/server/routes/facebookLogin.js
@@ -1,24 +1,78 @@
 var express = require('express');
 var router = express.Router();
 var FB = require('fb');
+const User = require('../models/User');
 
 router.post('/', function (req, res) {
-	FB.api('/me', { fields: 'id,name', access_token: req.body.accessToken }, function (response) {
+	FB.api('/me', { fields: 'id,name', access_token: req.body.fbAccessToken }, function (response) {
     if(response && response.error) {
-      if(response.error.code === 'ETIMEDOUT') {
-          console.log('request timeout');
-      }
-      else {
-          console.log('error', response.error);
-      }
-			// TODO: send error message
-			res.send(response);
+			// Error 0: Response error from Facebook request
+			res.send({
+				success: false,
+				msg: "Unable to log in. Please contact an admin. (Error 0)"
+			});
     }
-    else if (response.id === req.body.userId) {
-			res.send('Success: Create/Return User and Session/Token');
+    else if (response.id === req.body.fbUserId) {
+			User.find({fbUserId: req.body.fbUserId}).exec(function (err, user) {
+			  if (err) {
+					// Error 1: Problem querying database
+					res.send({
+						success: false,
+						msg: "Unable to log in. Please contact an admin. (Error 1)"
+					});
+				} else if (user.length === 0) {
+					// Success: First Time / Create Record
+					var userDetail = {
+						name: response.name,
+						fbUserId: response.id,
+						favorites: []
+					};
+					var user = new User(userDetail);
+					user.save(function (err) {
+					  if (err) {
+							// Error 2: Problem saving to database
+							res.send({
+								success: false,
+								msg: "Unable to log in. Please contact an admin. (Error 2)"
+							});
+					  } else {
+							res.send({
+								success: true,
+								new: true,
+								user: {
+									id: user._id,
+									name: user.name,
+									favorites: user.favorites
+								}
+							});
+						}
+					});
+				} else if (user.length > 1) {
+					// Error 3: More than 1 user with same Facebook ID
+					res.send({
+						success: false,
+						msg: "Unable to log in. Please contact an admin. (Error 3)"
+					});
+				} else {
+					// Success: Already a User
+					res.send({
+						success: true,
+						new: false,
+						msg: "Successfully logged in.",
+						user: {
+							id: user[0]._id,
+							name: user[0].name,
+							favorites: user[0].favorites
+						}
+					});
+				}
+			})
     } else {
-			// TODO: userId doesn't match
-			res.send('Invalid');
+			// Error 4: User tried logging in but IDs don't match (hacking?)
+			res.send({
+				success: false,
+				msg: "Unable to log in. Please contact an admin. (Error 4)"
+			});
 		}
 	});
 })
