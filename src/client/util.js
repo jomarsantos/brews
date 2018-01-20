5c5f6c664fdd8180464442b00d8d63e66598c252
Jomar Santos <jomaroliversantos@gmail.com>
2018-01-20 13:53:18 -0800

    fix update date

--- src/client/util.js
+++ src/client/util.js
@@ -1,5 +1,5 @@
 exports.formatDate = function(rawDate) {
 	let date = new Date(rawDate);
-	return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear()
+	return (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear()
 		+ " " + date.getHours() + ":" + date.getMinutes() + " PST";
 };
