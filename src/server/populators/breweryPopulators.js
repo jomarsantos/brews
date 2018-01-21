4e30f1fd9feb6ebf8c35923a9cca5eff2fe0fe95
Jomar Santos <jomaroliversantos@gmail.com>
2018-01-20 21:25:20 -0800

    strathcona

--- src/server/populators/breweryPopulators.js
+++ src/server/populators/breweryPopulators.js
@@ -8,6 +8,7 @@ var parallel49 = require('./breweryPopulators/parallel49');
 var dogwood = require('./breweryPopulators/dogwood');
 var redtruck = require('./breweryPopulators/redtruck');
 var callister = require('./breweryPopulators/callister');
+var strathcona = require('./breweryPopulators/strathcona');
 
 function combineBreweryPopulators() {
 	// Property must match up with code of brewery
@@ -18,7 +19,8 @@ function combineBreweryPopulators() {
 		parallel49: parallel49,
 		dogwood: dogwood,
 		redtruck: redtruck,
-		callister: callister
+		callister: callister,
+		strathcona: strathcona
 	}
 
 	// To handle codes that start with a number
