6e367cdb81f94b52b70aa36f3d804bda130cdc67
Jomar Santos <jomaroliversantos@gmail.com>
2018-01-31 22:23:08 -0800

    add links to brewery detail

--- src/client/containers/BreweryDetailContainer.js
+++ src/client/containers/BreweryDetailContainer.js
@@ -9,11 +9,53 @@ class BreweryDetailContainer extends Component {
 				<h1>Loading...</h1>
 			);
 		} else {
+			let links = [];
+			if (this.props.details.hasOwnProperty('website')) {
+				links.push(
+					<a class='breweryDetailContainer-link' id='breweryDetail_website' href={this.props.details.website} target="_blank">
+						<i className="breweryDetailContainer-linkIcon fa fa-globe" aria-hidden="true"></i>
+					</a>
+				);
+			}
+			if (this.props.details.hasOwnProperty('facebook')) {
+				links.push(
+					<a class='breweryDetailContainer-link' id='breweryDetail_facebook' href={this.props.details.facebook} target="_blank">
+						<i className="breweryDetailContainer-linkIcon fa fa-facebook-square" aria-hidden="true"></i>
+					</a>
+				);
+			}
+			if (this.props.details.hasOwnProperty('instagram')) {
+				links.push(
+					<a class='breweryDetailContainer-link' id='breweryDetail_instagram' href={'https://www.instagram.com/'+this.props.details.instagram} target="_blank">
+						<i className="breweryDetailContainer-linkIcon fa fa-instagram" aria-hidden="true"></i>
+					</a>
+				);
+			}
+			if (this.props.details.hasOwnProperty('twitter')) {
+				links.push(
+					<a class='breweryDetailContainer-link' id='breweryDetail_twitter' href={'https://twitter.com/'+this.props.details.twitter} target="_blank">
+						<i className="breweryDetailContainer-linkIcon fa fa-twitter-square" aria-hidden="true"></i>
+					</a>
+				);
+			}
+			if (this.props.details.hasOwnProperty('youtube')) {
+				links.push(
+					<a class='breweryDetailContainer-link' id='breweryDetail_youtube' href={this.props.details.youtube} target="_blank">
+						<i className="breweryDetailContainer-linkIcon fa fa-youtube-square" aria-hidden="true"></i>
+					</a>
+				);
+			}
+			if (links.length === 0) {
+				links = null;
+			}
 			main = (
 				<div>
 					<img id='breweryDetailContainer-image' src={this.props.details.logo}/>
 					<br />
-					<h1 id ='breweryDetailContainer-name'>{this.props.details.name}</h1>
+					<div>
+						<h1 id ='breweryDetailContainer-name'>{this.props.details.name}</h1>
+						{links}
+					</div>
 				</div>
 			);
 		}
